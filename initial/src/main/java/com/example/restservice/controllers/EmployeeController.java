5e778ecbdbf3482d66bf6033cfdbc7b7eda44880
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-12-04 17:32:35 +0100

    Added Post request

--- initial/src/main/java/com/example/restservice/controllers/EmployeeController.java
+++ initial/src/main/java/com/example/restservice/controllers/EmployeeController.java
@@ -2,10 +2,13 @@ package com.example.restservice.controllers;
 
 
 import com.example.restservice.data.EmployeeManager;
+import com.example.restservice.entities.Employee;
 import com.example.restservice.storage.Employees;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(path = "/employees")
@@ -17,5 +20,33 @@ public class EmployeeController {
     public Employees getEmployees()
     {
         return employeeDAO.getAllEmployees();
+    }
+
+    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(
+            @RequestBody Employee employee)
+    {
+
+        // Creating an ID of an employee
+        // from the number of employees
+        Integer id = employeeDAO
+                .getAllEmployees()
+                .getEmployeeList()
+                .size()
+                + 1;
+
+        employee.setEmployee_id(id);
+
+        employeeDAO.addEmployee(employee);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(
+                        employee.getEmployee_id())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .build();
     }
 }
